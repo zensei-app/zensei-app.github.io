0baac09f360dd6122101022329d1f5b33345878b
David Martin-Corral <david@zenseiapp.com>
2019-03-08 21:14:13 +0100

    Update blog header

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,6 +39,26 @@ window.twttr = (function(d, s, id) {
 }(document, "script", "twitter-wjs"));
 
 
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("blog-header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
 // End social
 
 // Begin Mixpanel
@@ -264,4 +284,5 @@ function post_to_url(path, params, method) {
   form._submit_function_(); // Call the renamed function.
 
 }
+
 
