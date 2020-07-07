beee90b391e3d98bff1c1be3d8eeab9237d6883b
Maria Garcia de Lomana <mariagarciadelomana@gmail.com>
2020-07-07 09:58:09 +0200

    add carousel feature and update blog header/footer

--- assets/js/home.js
+++ assets/js/home.js
@@ -31,4 +31,89 @@ $(document).ready(function () {
       2000
     );
   });
+
+  //https://kenwheeler.github.io/slick/
+  $(".carousel-responsive").slick({
+    accesibility: true,
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+
+  //https://kenwheeler.github.io/slick/
+  $(".opinion-carousel-responsive").slick({
+    accesibility: true,
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+
+  //drop-down in slide4
+  $("#open-drop-down").click(function () {
+    $("#open-drop-down-btn").addClass("invisible");
+    $("#close-drop-down-btn").removeClass("invisible");
+    $("#symptoms-list").removeClass("home--slide4__list1-closed");
+  });
+
+  $("#close-drop-down").click(function () {
+    $("#close-drop-down-btn").addClass("invisible");
+    $("#open-drop-down-btn").removeClass("invisible");
+    $("#symptoms-list").addClass("home--slide4__list1-closed");
+  });
 });
